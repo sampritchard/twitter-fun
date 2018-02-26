57edefb3236566246d9cc69e4548cc591f46d7f9
Sam Pritchard <sampritchard@msn.com>
2018-02-26 22:21:16 +0000

    trying to pass first test

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,1 +1,14 @@
-//TO DO - UNIT TESTS
+let chai = require('chai');
+let expect = chai.expect;
+let sinon = require("sinon");
+let updateTwitter = require('../../app.js');
+let updateStatus = require('../../app.js');
+
+describe('#updateTwitter', function () {
+
+    it('should call updateStatus', function () {
+       let mySpy = sinon.spy(updateStatus);
+       updateTwitter;
+       expect(mySpy).calledOnce;
+    });
+});
